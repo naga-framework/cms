a397da2a9030695478e10c8c6acb44a8f39cdf2e
mihawk <chan.sisowath@gmail.com>
2017-02-20 18:06:15 +0700

    add model author

--- apps/cms/src/cms_db.erl
+++ apps/cms/src/cms_db.erl
@@ -40,20 +40,20 @@ new(T,Attrs) when is_map(Attrs)->
     lists:foldl(fun({F,V},N)->?MODULE:set_value(F,V,N)end,R,maps:to_list(Attrs)).
 
 
-% attribute_names(T) -> ?MODULE:field_names(T).
-% attribute_types(T) -> ?MODULE:field_types(T).
+attribute_names(T) -> ?MODULE:field_names(T).
+attribute_types(T) -> ?MODULE:field_types(T).
 
-% attributes(R) when is_tuple(R) -> 
-%     Fields = ?MODULE:field_names(element(1,R)),
-%     lists:foldr(fun(F,Acc) -> 
-%                     [{F, ?MODULE:get_value(F,R)}|Acc] 
-%                 end, [], Fields).
+attributes(R) when is_tuple(R) -> 
+    Fields = ?MODULE:field_names(element(1,R)),
+    lists:foldr(fun(F,Acc) -> 
+                    [{F, ?MODULE:get_value(F,R)}|Acc] 
+                end, [], Fields).
 
-% attribute_idx(T) ->
-%     Fields = ?MODULE:field_names(T),
-%     lists:zip(Fields,lists:seq(1,length(Fields))).
+attribute_idx(T) ->
+    Fields = ?MODULE:field_names(T),
+    lists:zip(Fields,lists:seq(1,length(Fields))).
 
-% to_maps(R) when is_tuple(R)-> maps:from_list(attributes(R)).
-% to_json(R) -> T=element(1,R),
-%               ?JSON({T:prepare_json(attributes(R))}).
+to_maps(R) when is_tuple(R)-> maps:from_list(attributes(R)).
+to_json(R) -> T=element(1,R),
+              ?JSON({T:prepare_json(attributes(R))}).
 

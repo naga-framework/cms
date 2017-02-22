29325116b769b3f274149a8d3fd2ef3b81fccf71
mihawk <chan.sisowath@gmail.com>
2017-02-23 00:04:37 +0700

    split tpl into tag,layout

--- apps/cms/src/controller/adm_dashboard.erl
+++ apps/cms/src/controller/adm_dashboard.erl
@@ -1,6 +1,6 @@
 -module(adm_dashboard).
--export([index/3 
-         %event/1
+-export([index/3, 
+         event/1
         ]).
 -default_action(index).
 -actions([index]).
@@ -8,12 +8,38 @@
 -include_lib("n2o/include/wf.hrl").
 -include_lib("nitro/include/nitro.hrl").
 -include_lib("naga/include/naga.hrl").
+-include("cms.hrl").
 
 %--------------------------------------------------------------------------------
 % CONTROLLER
 %--------------------------------------------------------------------------------
-index(<<"GET">>, _, _)   -> 
-  {ok, []}.
+index(<<"GET">>, _, #{identity:=Identity} = Ctx)   -> 
+  io:format("Identity ~p~n",[Identity]),
+  {ok, bindings(Identity)}.
+
+
+bindings(#{user:=User}) ->
+  VendorsCSS = [bootstrap3,fontawesome,
+                nprogress,icheck,progressbar,
+                jqvmap,moment,daterangepicker,
+                pnotify,gentelella],
+  VendorsJS = [jquery,bootstrap3,fastclick,
+                nprogress,chartjs,gauge,
+                progressbar,icheck,skycons,flot,
+                flot_orderbars,flot_spline,flot_curvedlines,
+                datejs,jqvmap,moment,
+                daterangepicker,pnotify,gentelella],                
+  CSS = gentelella:vendors(css,VendorsCSS),
+  JS  = gentelella:vendors(js,VendorsJS),
+   [
+    {user,User:attributes()},
+    {app, [{name,?APP_NAME},{vsn,?APP_VSN}]},
+    {page,[{css,CSS},
+           {title,"Dashboard"},
+           {js,JS}]}
+   ].
+
+
 
 event(Event) -> 
   wf:info(?MODULE,"Unknown Event: ~p~n",[Event]).

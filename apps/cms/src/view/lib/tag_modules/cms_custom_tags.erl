115544c1fb7adee7b37ebecabe14b8a1b36862cb
mihawk <chan.sisowath@gmail.com>
2017-02-22 15:46:12 +0700

    rem dbg

--- apps/cms/src/view/lib/tag_modules/cms_custom_tags.erl
+++ apps/cms/src/view/lib/tag_modules/cms_custom_tags.erl
@@ -4,7 +4,7 @@
 % put custom tags in here, e.g.
 %
 reverse(Variables, Options) ->
-    io:format("Variables: ~p, Options: ~p~n", [Variables, Options] ),
+    %io:format("Variables: ~p, Options: ~p~n", [Variables, Options] ),
     lists:reverse(binary_to_list(proplists:get_value(string, Variables))).
 %
 % {% reverse string="hello" %} => "olleh"

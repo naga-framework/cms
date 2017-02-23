d89d372fb8055285a8a9db8673571d80aff64018
mihawk <chan.sisowath@gmail.com>
2017-02-23 09:51:18 +0700

    add tag my_sidebar_menu

--- apps/cms/src/view/lib/tag_modules/cms_custom_tags.erl
+++ apps/cms/src/view/lib/tag_modules/cms_custom_tags.erl
@@ -1,6 +1,11 @@
 -module(cms_custom_tags).
 -compile(export_all).
 
+-include_lib("n2o/include/wf.hrl").
+-include_lib("naga/include/naga.hrl").
+-include_lib("cms/include/cms.hrl").
+-include_lib("nitro/include/nitro.hrl").
+
 % put custom tags in here, e.g.
 %
 reverse(Variables, Options) ->
@@ -10,3 +15,45 @@ reverse(Variables, Options) ->
 % {% reverse string="hello" %} => "olleh"
 %
 % Variables are the passed-in vars in your template
+
+my_sidebar_menu(_Vars,_Opts) ->
+ wf:render(
+  #panel{id= <<"sidebar-menu">>, class=["main_menu_side hidden-print main_menu"], body=[
+    #panel{class=[menu_section],body=[
+      #h3{body=["CMS"]}
+    ]},
+    #ul{class=["nav side-menu"], body=[
+      #li{body=[
+        #link{ href="/admin",body=[
+          #i{class=["fa fa-dashboard"]},
+          "Dashboard"
+        ]}
+      ]},          
+      #li{body=[
+        #link{body=[
+          #i{class=["fa fa-list"]},
+          "Articles"
+        ]},
+        #ul{class=["nav child_menu"], body=[
+          #li{body=[
+            #link{href="/admin/articles",body=["All Articles"]}
+          ]},
+          #li{body=[
+            #link{href="/admin/article/add",body=["Add New"]}
+          ]}
+        ]}        
+      ]},
+      #li{body=[
+        #link{ href="/admin/comments",body=[
+          #i{class=["fa fa-comments"]},
+          "Comments"
+        ]}
+      ]},
+      #li{body=[
+        #link{ href="/admin/profile",body=[
+          #i{class=["fa fa-user"]},
+          "Profile"
+        ]}
+      ]}    
+    ]}
+  ]}).  

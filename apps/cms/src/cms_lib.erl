7bdc6397fda57b8ee4fecb674b1d0860fe707f98
mihawk <chan.sisowath@gmail.com>
2017-02-24 14:29:48 +0700

    add role to user info

--- apps/cms/src/cms_lib.erl
+++ apps/cms/src/cms_lib.erl
@@ -6,17 +6,24 @@
 -include_lib("nitro/include/nitro.hrl").
 -include_lib("naga/include/naga.hrl").
 -include("cms.hrl").
+-define(CREDIT,"CMS with Gentelella "
+               "Bootstrap Admin Template, powered by "
+               "<a href='http://github.com/naga-framework/naga'>naga-framework</a>").
 
-bindings(#{user:=User}, VendorsCSS, VendorsJS) ->               
+bindings(Identity, VendorsCSS, VendorsJS) ->               
   CSS = gentelella:vendors(css,VendorsCSS),
   JS  = gentelella:vendors(js,VendorsJS),
+  #{is_admin := IsAdmin,
+    is_author:= IsAuthor,
+    is_moderator:= IsModerator,
+    user:=User} = Identity,
+  Access = [{id_admin, IsAdmin},{is_moderator,IsModerator},{is_author,IsAuthor}],
+  io:format("Access ~p~n",[Access]),
    [
-    {user,User:attributes()},
+    {user,User:attributes()++[{access,Access}]},
     {app, [{name,?APP_NAME},
            {vsn,?APP_VSN},
-           {credit, "CMS with Gentelella "
-                    "Bootstrap Admin Template, powered by "
-                    "<a href='http://github.com/naga-framework/naga'>naga-framework"}
+           {credit, ?CREDIT}
            ]},
     {page,[{css,CSS},
            {title,"CMS Demo"},
@@ -29,9 +36,7 @@ bindings(VendorsCSS, VendorsJS) ->
    [
     {app, [{name,?APP_NAME},
            {vsn,?APP_VSN},
-           {credit, "CMS with Gentelella "
-                    "Bootstrap Admin Template, powered by "
-                    "<a href='http://github.com/naga-framework/naga'>naga-framework"}
+           {credit, ?CREDIT}
            ]},
     {page,[{css,CSS},
            {title,"CMS Demo"},
